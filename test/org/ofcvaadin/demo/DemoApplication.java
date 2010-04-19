383df181b532d9243c940fb55777c9154102be6a
jjplion <jjplion@7b17f5cf-0b4a-70f0-b7c5-0c62f6b474a7>
2010-04-19 16:16:45 +0000

    version 0.1

---
+++ test/org/ofcvaadin/demo/DemoApplication.java
@@ -0,0 +1,184 @@
+package org.ofcvaadin.demo;
+
+import java.util.ArrayList;
+
+import jofc2.OFC;
+import jofc2.model.Chart;
+import jofc2.model.axis.XAxis;
+import jofc2.model.axis.YAxis;
+import jofc2.model.elements.BarChart;
+import jofc2.model.elements.HorizontalBarChart;
+import jofc2.model.elements.LineChart;
+import jofc2.model.elements.PieChart;
+
+import org.ofcvaadin.ui.OpenFlashChart.ChartDataGenerator;
+
+import com.vaadin.Application;
+import com.vaadin.ui.*;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
+
+public class DemoApplication extends Application {
+	private static final long serialVersionUID = 4422477701385760785L;
+
+	@Override
+	public void init() {
+		Window mainWindow = new Window("Open Flash Chart for Vaadin");
+		Label label = new Label("Open Flash Chart for Vaadin demo. More cases are coming." +
+				"Please visit page for more details:" +
+				"<a href='http://code.google.com/p/ofcvaadin/'>ofcvaadin</a>",
+				Label.CONTENT_XHTML);
+		mainWindow.addComponent(label);
+
+		Button showDemos = new Button("Show All Demos");
+		showDemos.addListener(new ClickListener(){
+			private static final long serialVersionUID = -89669779516839869L;
+
+			@Override
+			public void buttonClick(ClickEvent event) {
+				showAllDemoWindows();
+			}
+		});
+		mainWindow.addComponent(showDemos);
+		
+		setMainWindow(mainWindow);
+	}
+	
+	private void showAllDemoWindows(){
+		getMainWindow().addWindow(createLineChartDemo());
+		getMainWindow().addWindow(createBarChartDemo());
+		getMainWindow().addWindow(createHorizontalBarChartDemo());
+		getMainWindow().addWindow(createPieChartDemo());
+	}
+	
+	@SuppressWarnings("serial")
+	private Window createLineChartDemo(){
+		final String title = "Line Chart";
+		OneChartWindow chartWindow = new OneChartWindow(this, title);
+		chartWindow.setChartDataGenerator(new ChartDataGenerator(){
+			@Override
+			public String getJson() {
+				LineChart lc = new LineChart();
+				lc.setText("test_1");
+
+				ArrayList<Number> lst = new ArrayList<Number>();
+				for (int i = 0; i < 10; i++) {
+					lst.add((int) (Math.random() * 20));
+				}
+				lc.addValues(lst);
+
+				YAxis ya = new YAxis();
+				ya.setMax(22);
+				ya.setSteps(2);
+
+				Chart c = new Chart(title);
+				c.addElements(lc);
+				c.setYAxis(ya);
+
+				String s = OFC.getInstance().prettyPrint(c, 4);
+				return s;
+			}
+		});
+		
+		return chartWindow;
+	}
+	
+	private Window createBarChartDemo(){
+		final String title = "Bar Chart";
+		OneChartWindow chartWindow = new OneChartWindow(this, title);
+		chartWindow.setChartDataGenerator(new ChartDataGenerator(){
+			private static final long serialVersionUID = 3164548364271914786L;
+
+			@Override
+			public String getJson() {
+				BarChart lc = new BarChart();
+				lc.setText("test_1");
+
+				ArrayList<Number> lst = new ArrayList<Number>();
+				for (int i = 0; i < 10; i++) {
+					lst.add((int) (Math.random() * 20));
+				}
+				lc.addValues(lst);
+
+				YAxis ya = new YAxis();
+				ya.setMax(22);
+				ya.setSteps(2);
+
+				Chart c = new Chart(title);
+				c.addElements(lc);
+				c.setYAxis(ya);
+
+				String s = OFC.getInstance().prettyPrint(c, 4);
+				return s;
+			}
+		});
+		
+		return chartWindow;
+	}
+	
+	private Window createHorizontalBarChartDemo(){
+		final String title = "Horizontal Bar Chart";
+		OneChartWindow chartWindow = new OneChartWindow(this, title);
+		chartWindow.setChartDataGenerator(new ChartDataGenerator(){
+			private static final long serialVersionUID = -4139782544095092771L;
+
+			@Override
+			public String getJson() {
+				HorizontalBarChart lc = new HorizontalBarChart();
+				lc.setText("test_1");
+
+				ArrayList<Number> lst = new ArrayList<Number>();
+				for (int i = 0; i < 10; i++) {
+					lst.add((int) (Math.random() * 20));
+				}
+				lc.addValues(lst);
+
+				XAxis xa = new XAxis();
+				xa.setMax(22);
+				xa.setSteps(2);
+
+				Chart c = new Chart(title);
+				c.addElements(lc);
+				c.setXAxis(xa);
+
+				String s = OFC.getInstance().prettyPrint(c, 4);
+				return s;
+			}
+		});
+		
+		return chartWindow;
+	}
+	
+	private Window createPieChartDemo(){
+		final String title = "Pie Chart";
+		OneChartWindow chartWindow = new OneChartWindow(this, title);
+		chartWindow.setChartDataGenerator(new ChartDataGenerator(){
+			private static final long serialVersionUID = 5732132570890663242L;
+
+			@Override
+			public String getJson() {
+				PieChart lc = new PieChart();
+				lc.setText("test_1");
+
+				ArrayList<Number> lst = new ArrayList<Number>();
+				for (int i = 0; i < 10; i++) {
+					lst.add((int) (Math.random() * 20));
+				}
+				lc.addValues(lst);
+
+				YAxis ya = new YAxis();
+				ya.setMax(22);
+				ya.setSteps(2);
+
+				Chart c = new Chart(title);
+				c.addElements(lc);
+				c.setYAxis(ya);
+
+				String s = OFC.getInstance().prettyPrint(c, 4);
+				return s;
+			}
+		});
+		
+		return chartWindow;
+	}
+}
